ba0a78f3263e40141405e26175a7fca6cc20e7ca
mdtcgitw <md@tcgitw.com>
2022-02-02 18:15:28 +0530

    Add project files.

---
+++ Scripts/PageScripts/index.js
@@ -0,0 +1,22 @@
+﻿
+
+var GetLogin = function () {
+    var username = $('#txtUsername').val();
+    var password = $('#txtPassword').val();
+    var loginUrl = "/api/Values/GetLogin";
+    var loginData = JSON.stringify({ "Username": username, "Password": password });
+
+    $.ajax({
+        type: "POST",
+        data: loginData,
+        url: loginUrl,
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            alert(result)
+        },
+        error: function (result) {
+            alert(result)
+        }
+    });
+};
